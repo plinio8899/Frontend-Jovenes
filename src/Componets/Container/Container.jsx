2c780bdcc46fc701e26767b4cd28c8253f3aa0a6
plinio8899 <plinio8899@gmail.com>
2023-02-27 21:38:30 -0500

    Se realiza el primer commit del proyecto

---
+++ src/Componets/Container/Container.jsx
@@ -0,0 +1,7 @@
+export function Container(){
+    return(
+        <div className="container">
+            <h1>Container</h1>
+        </div>
+    )
+}
---
+++ src/Componets/Header/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css'
+
+export function Header(){
+    return(
+        <header>
+            <h2>Navigation</h2>
+        </header>
+    )
+} 
