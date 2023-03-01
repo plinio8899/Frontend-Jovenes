e3ca58b02e7ad2fbed4104aed4b7612915c8a43e
Neon Mebulock <nmebulok@gmail.com>
2023-02-28 21:14:28 -0500

    Se realizo el diseño inicial de el Header y de el Container,se puso el sector donde se van a mostrar los puntos

--- src/Componets/Header/Header.jsx
+++ src/Componets/Header/Header.jsx
@@ -1,9 +1,69 @@
+import { useState } from "react";
 import './Header.css'
 
-export function Header(){
-    return(
+
+
+export function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [activeOption, setActiveOption] = useState("OPcion1");
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleOptionClick = (option) => {
+    setActiveOption(option);
+
+  };
+
+
+  return (
+    <>
+      <nav>
         <header>
-            <h2>Navigation</h2>
+          <div className="navbar">
+            <a href="" className="logo">
+              <img src="../public/Logo.jpg" alt="Logo" />
+            </a>
+            <div className="nav-links">
+              <ul>
+                {/* {["OPcion1", "OPcion2", "OPcion3"].map((option) => (
+                  <li key={option}>
+                    <a  href={getUrlForOption(option)} className={activeOption === option ? "active" : ""} onClick={() => handleOptionClick(option)}>
+                      {option}
+                    </a>
+                  </li>
+                ))}*/}
+                <li><a href="#" className={activeOption === "option1" ? "active" : ""} onClick={() => handleOptionClick("option1")}>Option 1</a></li>
+                <li><a href="#" className={activeOption === "option2" ? "active" : ""} onClick={() => handleOptionClick("option2")}>Option 2 </a></li>
+                <li><a href="#" className={activeOption === "option3" ? "active" : ""} onClick={() => handleOptionClick("option3")}>Option 3 </a></li>
+              </ul>
+            </div>
+            <div className="toggle" onClick={toggleMenu}></div>
+          </div>
         </header>
-    )
-} 
+        {showMenu && (
+          <div className="mobile-menu">
+            <div className="nav-links-mobile">
+              <ul>
+                {/* {["OPcion1", "OPcion2", "OPcion3"].map((option) => (
+                  <li key={option}>
+                    <a href="/" className={activeOption === option ? "active" : ""} onClick={() => handleOptionClick(option)}>
+                      {option}
+                    </a>
+                  </li>
+                ))} */}
+                <li><a href="#" className={activeOption === "option1" ? "active" : ""} onClick={() => handleOptionClick("option1")}>Option 1</a></li>
+                <li><a href="#" className={activeOption === "option2" ? "active" : ""} onClick={() => handleOptionClick("option2")}>Option 2 </a></li>
+                <li><a href="#" className={activeOption === "option3" ? "active" : ""} onClick={() => handleOptionClick("option3")}>Option 3 </a></li>
+              </ul>
+            </div>
+            <div className="toggle togglePst" onClick={toggleMenu}></div>
+          </div>
+        )}
+      </nav>
+    </>
+  )
+}
+
+
