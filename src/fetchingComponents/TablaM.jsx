cda6b354656168b12b1c7f42c720c27abe0342eb
Chilli-Code <jorgexc42@gmail.com>
2023-03-07 12:33:08 -0500

    Se Comento los svg que habian en el container, se instalo una nueva dependencia para los SVG animados de Lottie.js, se creo una nueva carpeta llamada svgs en la cual contiene los archivos json que son los svgs, se quitaron unos errores de consola para limpiarla

--- src/fetchingComponents/TablaM.jsx
+++ src/fetchingComponents/TablaM.jsx
@@ -4,7 +4,6 @@ export function TablaM(){
     const url = "https://backend-ipc-jov.onrender.com/m-points/"
     const [desc, setDesc] = useState(null)
 
-
     useEffect(() =>{
         fetch(url)
         .then((res) => res.json())
@@ -13,20 +12,23 @@ export function TablaM(){
         })
     }, [])
 
-
     return(
         <>
             <div className="tableTeam tableTeamPadd">
                 {desc?.map((tabla) => (
                     <>
-                    <table   cellpadding="2" width="100%">
+                    <table  cellPadding="2" width="100%">
+                    <tbody>
                         <tr>
                             <td style={{textAlign: "center"}} width="18%">{tabla.number}</td>
-                            <td  rowspan="2" colspan="2" cellpadding="2">{tabla.description}</td>
+                            <td  rowSpan="2" colSpan="2" cellPadding="2">{tabla.description}</td>
                         </tr>
+                        </tbody>
                     </table>
                     </>))}
                 </div>
             </>
         )
 }
+
+
